ca49b1ce79bae904ab7dc13df03acb53e7c00a01
boybook <fangyizhou1997@qq.com>
2023-02-13 23:40:17 +0800

    改名

--- rank-matcher-client-j/src/java/net/easecation/rankmatcher/api/message/MatchSuccessMessage.java
+++ rank-matcher-client-j/src/java/net/easecation/rankmatcher/api/message/MatchSuccessMessage.java
@@ -6,21 +6,21 @@ import net.easecation.rankmatcher.api.MessageType;
 public class MatchSuccessMessage implements Message {
 
     private String arena;
-    private String[] player;
+    private String[] players;
 
     public static MatchSuccessMessage of(String arena, String[] player) {
         MatchSuccessMessage message = new MatchSuccessMessage();
         message.arena = arena;
-        message.player = player;
+        message.players = player;
         return message;
     }
 
     @Override
     public void decode(String[] data) {
         arena = data[3];  // 2 3
-        player = new String[Integer.parseInt(data[4])];
-        for (int i = 0; i < player.length; i++) {
-            player[i] = data[5 + 1 + i * 2];
+        players = new String[Integer.parseInt(data[4])];
+        for (int i = 0; i < players.length; i++) {
+            players[i] = data[5 + 1 + i * 2];
         }
     }
 
@@ -28,8 +28,8 @@ public class MatchSuccessMessage implements Message {
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append(PROTOCOL_VERSION).append(",").append(getMessageType().getTypeId()).append(",").append(Message.writeString(arena));
-        sb.append(",").append(player.length);
-        for (String p : player) {
+        sb.append(",").append(players.length);
+        for (String p : players) {
             sb.append(",").append(Message.writeString(p));
         }
         return sb.toString();
